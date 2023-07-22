44ac9f35f723c034a5219410189eee454c0b4a37
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-22 11:45:32 -0300

    incluindo pausa antes do encerramento do programa

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from game_files.entities import *
 from game_files.encounter import *
 
+
 #nome
 lucas = Player('lucas')
 
@@ -11,4 +12,6 @@ slime = Enemy('Slime', 10.0, 1, 1)
 
 next_encounter = Encounter(lucas,slime)
 next_encounter.start()
+
+input()
 
