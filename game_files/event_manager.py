7bc0ef69141b7fda8a44ab5e667efd34b36f1cc7
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-22 19:38:01 -0300

    elaboração do event handler implementando encounters e o futuro shop

---
+++ game_files/event_manager.py
@@ -0,0 +1,14 @@
+from game_files.encounter import Encounter
+from game_files.shop import Shop
+
+class EventManager:
+    
+    def __init__(self, player):
+        self.player = player
+
+    def handle_encounter(self, enemy):
+        encounter = Encounter(self.player, enemy)
+        encounter.start()
+        
+    def visit_shop(self):
+        shop = Shop(self.player)
