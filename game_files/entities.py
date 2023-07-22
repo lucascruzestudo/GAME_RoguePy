8c7f0428caddcc94c3d8328dfd1d602025aad80e
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-22 19:02:09 -0300

    criação de formula básica de ataque e defesa, aleatorização de dano causado, renomeando atributos para attack e defense

--- game_files/entities.py
+++ game_files/entities.py
@@ -1,13 +1,13 @@
-from random import random
+from random import random, uniform
 from colorama import Fore, Style
 
 class Entity:
     
-    def __init__(self, name, health, damage, level):
+    def __init__(self, name, health, attack, defense):
         self._name = name.upper()
         self._health = health
-        self._damage = damage
-        self._level = level
+        self._attack = attack
+        self._defense = defense
         
     @property
     def name(self):
@@ -22,44 +22,52 @@ class Entity:
         self._health = new_value
     
     @property
-    def damage(self):
-        return self._damage
+    def attack(self):
+        return self._attack
     
     @property
-    def level(self):
-        return self._level
+    def defense(self):
+        return self._defense
     
     def is_alive(self):
         return self.health > 0
     
-    def attack(self, target):
+    def deal_damage(self, target):
             print(f"{self.name} attacks {target.name}!")
-            target.receive_damage(self.damage)
+            damage_range = uniform(0.8, 1.2)
+            mod_attack = self.attack * damage_range
+            target.receive_damage(mod_attack)
 
-    def receive_damage(self, amount):
-        formatted_damage = f"{Fore.RED}{Style.BRIGHT}{amount}{Style.RESET_ALL}"
-        print(f"{self.name} took {formatted_damage} damage.\n")
-        self.health -= amount
+    def receive_damage(self, attack):
+        attack = attack / self.defense
+        attack = round(attack * 4) / 4 
+        damage = f"{Fore.RED}{Style.BRIGHT}{attack}{Style.RESET_ALL}"
+        print(f"{self.name} took {damage} damage.\n")
+        self.health -= attack
     
 class Player(Entity):
-    def __init__(self, name, health=100.0, damage=1, level=1):
-        super().__init__(name, health, damage, level)
-        self._crit_odd = 1
+    def __init__(self, name, health, attack, defense):
+        super().__init__(name, health, attack, defense)
+        self._crit_odd = 0.05
         self._crit_amp = 1.25
         
     @staticmethod
     def _critical_hit_message():
         return Fore.RED + Style.BRIGHT + "CRITICAL HIT!" + Style.RESET_ALL
         
-    def attack(self, target):
+    def deal_damage(self, target):
         
             print(f"{self.name} attacks {target.name}!")
+            
+            damage_range = uniform(0.8, 1.2)
+            mod_attack = self.attack * damage_range
+            
             crit_floor = random()
             if crit_floor <= self.crit_odd:
                 print(self._critical_hit_message())
-                target.receive_damage(self.damage * self.crit_amp)
+                target.receive_damage(mod_attack * self.crit_amp)
             else:
-                target.receive_damage(self.damage)
+                target.receive_damage(mod_attack)
                 
     @property
     def name(self):
@@ -82,5 +90,5 @@ class Player(Entity):
         self._crit_amp = new_value
 
 class Enemy(Entity):
-    def __init__(self, name, health, damage, level):
-        super().__init__(name, health, damage, level)
+    def __init__(self, name, health, attack, defense):
+        super().__init__(name, health, attack, defense)
