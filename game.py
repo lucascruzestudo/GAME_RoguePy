9913fc629fc2867812742edb4283276dd78b6805
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-22 14:32:34 -0300

    melhoria dos menus de encontro e batalha

--- game.py
+++ game.py
@@ -1,17 +1,34 @@
 from game_files.entities import *
 from game_files.encounter import *
+import random
+from time import sleep
 
 
-#nome
-lucas = Player('lucas')
+# nome = input("Informe o nome do guerreiro: ")
+plr = Player('AA')
+print("Prepare-se para a maior batalha de sua vida...")
+sleep(1)
+
+# inimigos
 
 #nome, vida, dano, nivel
-slime = Enemy('Slime', 10.0, 1, 1)
+enemies_list = [
+    Enemy('Slime', 10.0, 1, 1),
+    Enemy('Ogre', 5.0, 1, 2),
+]
 
 
+while plr.is_alive() and enemies_list:
+    
+    selected_enemy = random.choice(enemies_list)
+    
+    enemies_list.remove(selected_enemy)
+    
+    encounter = Encounter(plr, selected_enemy)
+    encounter.start()
+    del encounter
 
-next_encounter = Encounter(lucas,slime)
-next_encounter.start()
-
-input()
-
+if plr.is_alive():
+    print(f"{plr.name} SURVIVED THE HORDE!")
+else:
+    print(f"{plr.name} DIED!")
--- game_files/encounter.py
+++ game_files/encounter.py
@@ -1,18 +1,97 @@
+from colorama import Fore, Style
+from time import sleep
+import os
+
+PAUSE_DURATION = 3
+
 class Encounter:
+    
+    @staticmethod
+    def clear():
+        sleep(PAUSE_DURATION)
+        os.system('cls||clear')
+
     def __init__(self, player, enemy):
         self.player = player
         self.enemy = enemy
+        self.round_counter = 0
+        self._fled = False
+        self.PAUSETIME = 2
+        
+    @property
+    def fled(self):
+        return self._fled
+    
+    @fled.setter
+    def fled(self, value):
+        self._fled = value
+
+    def print_status(self):
+        
+        print(f"{self.player.name} HP: {Fore.RED}{self.player.health}{Style.RESET_ALL}")
+        print(f"{self.enemy.name} HP: {Fore.RED}{self.enemy.health}{Style.RESET_ALL}\n")
+
+    def player_turn(self):
+        
+        print("Choose your action:")
+        print(Style.BRIGHT)
+        print(f"1. Attack")
+        print(f"2. Flee")
+        print(Style.RESET_ALL)
+
+        while True:
+            choice = input("Enter your choice: ")
+            
+            self.clear()
+            
+            if choice == "1":
+                self.player.attack(self.enemy)
+    
+                break
+            elif choice == "2":
+                self.fled = True
+                break
+            else:
+                print("Invalid choice. Try again.")
+
+    def enemy_turn(self):
+        
+        self.enemy.attack(self.player)
+            
+
 
     def start(self):
-        print(f"A {self.enemy.name} engages!\n")
-        while self.player.is_alive() and self.enemy.is_alive():
-            self.player.attack(self.enemy)
-            if self.enemy.is_alive():
-                self.enemy.attack(self.player)
+        
+        self.clear()
+
+        print(f"{Fore.YELLOW}{Style.BRIGHT}{self.enemy.name}{Style.RESET_ALL} engages!\n")
+
+        while self.player.is_alive() and self.enemy.is_alive() and self.fled == False:
+            
+            self.clear()
+            
+            self.round_counter += 1
+
+            print(f"--- Round {self.round_counter} ---")
+            self.print_status()
+            
+            self.player_turn()
+
+            if self.enemy.is_alive() and not self.fled:
+                self.enemy_turn()
+            
         self.end()
 
     def end(self):
-        if self.player.is_alive():
+        
+        os.system('cls||clear')
+        
+        if self.fled == True:
+            print(f"{self.player.name} fled the battle.")
+        elif self.player.is_alive():
             print(f"{self.player.name} has defeated {self.enemy.name}!")
         else:
             print(f"{self.enemy.name} has defeated {self.player.name}!")
+            
+        self.clear()
+            
