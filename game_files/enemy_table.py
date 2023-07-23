9d976565bc4451778d7c72520ef954252e09f169
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-23 14:29:05 -0300

    revisão da tabela de inimigos separando em andares e passando parametros ao inves de gerar instancias

--- game_files/enemy_table.py
+++ game_files/enemy_table.py
@@ -1,19 +1,13 @@
 from game_files.entities import Enemy
 
-enemies = [
-    Enemy(name='Slime', health=15.0, attack=3.5, defense=0.5),
-    Enemy(name='Ogre', health=30.0, attack=6.0, defense=1.5),
-    Enemy(name='Goblin', health=20.0, attack=4.5, defense=0.8),
-    Enemy(name='Harpy', health=25.0, attack=6.0, defense=1.2),
-    Enemy(name='Treant', health=40.0, attack=3.5, defense=2.0),
-    Enemy(name='Skeleton', health=18.0, attack=4.0, defense=0.7),
-    Enemy(name='Wisp', health=12.0, attack=6.5, defense=0.2),
-    Enemy(name='Cyclops', health=50.0, attack=3.0, defense=2.0),
-    Enemy(name='Centaur', health=28.0, attack=5.0, defense=1.0),
-    Enemy(name='Basilisk', health=35.0, attack=7.0, defense=1.8),
-    Enemy(name='Phoenix', health=45.0, attack=8.0, defense=1.5),
-    Enemy(name='Manticore', health=38.0, attack=6.2, defense=2.2),
-    Enemy(name='Griffin', health=32.0, attack=6.5, defense=1.7),
-    Enemy(name='Dryad', health=22.0, attack=4.5, defense=1.3),
-    Enemy(name='Dragonling', health=60.0, attack=9.0, defense=2.8),
-]
+floors = {
+    1: [
+        {'name': 'Slime', 'health': 15.0, 'attack': 2.5, 'defense': 0.5},
+        {'name': 'Ogre', 'health': 30.0, 'attack': 5.0, 'defense': 1.5}
+    ],
+    
+    2: [
+        {'name': 'Skeleton', 'health': 8.5, 'attack': 10.5, 'defense': 0.5},
+        {'name': 'Witch', 'health': 15.0, 'attack': 5.0, 'defense': 3}
+    ]
+}
