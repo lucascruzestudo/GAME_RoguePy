01fff3d49476b515c3b330fb04545a5bb5536493
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-23 14:28:05 -0300

    reorganização do fluxo de jogo, incubindo ao event manager cuidar do andamento do jogo

--- game.py
+++ game.py
@@ -1,15 +1,14 @@
 from game_files.entities import *
 from game_files.enemy_table import enemies
 from game_files.event_manager import EventManager
-from colorama import Fore, Style, init as colorama_init
 import random, os
 from time import sleep
 
-colorama_init()
+
 
 def game_setup():
     nome = input("Warrior name: ")
-    plr = Player(name=nome, health=100.0, attack=10, defense=2)
+    plr = Player(name=nome, health=100.0, attack=5, defense=2)
     return plr
 
 plr = game_setup()
@@ -17,33 +16,7 @@ plr_manager=EventManager(plr)
 os.system('clear||cls')
 
 
-while plr.is_alive() and enemies:
-    
-    selected_enemy = random.choice(enemies)
-    
-    enemies.remove(selected_enemy)
-    
-    plr_manager.handle_encounter(selected_enemy)
+plr_manager.start()
 
-if plr.is_alive():
-    print(f"{plr.name} ESCAPED THE DUNGEON!")
-    
-else:
-    print(f"{plr.name} died inside the dungeon.")
-    
-    img = """
-       @@@@@@@@@@@@@@@@@@
-     @@@@@@@@@@@@@@@@@@@@@@@
-   @@@@@@@@@@@@@@@@@@@@@@@@@@@
-  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
- @@@@@@@@@@@@@@@/      \@@@/   @
-@@@@@@@@@@@@@@@@\      @@  @___@
-@@@@@@@@@@@@@ @@@@@@@@@@  | \@@@@@
-@@@@@@@@@@@@@ @@@@@@@@@\__@_/@@@@@
- @@@@@@@@@@@@@@@/,/,/./'/_|.\'\\\\
-   @@@@@@@@@@@@@|  | | | | | | | |
-                 \_|_|_|_|_|_|_|_|
-    """
-    print(f"{Fore.RED}{Style.BRIGHT}{img}{Style.RESET_ALL}")
-
-input()
+print('\n')
+input("Press any key to exit...")
