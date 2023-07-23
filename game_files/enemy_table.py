fb027b989af25040522afcb90d1608050dfcbafa
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-23 16:52:18 -0300

    mais monstros !!

--- game_files/enemy_table.py
+++ game_files/enemy_table.py
@@ -1,10 +1,18 @@
 floors = {
     1: [
-        {"name": "Slime", "health": 15.0, "attack": 2.5, "defense": 0.5},
+        {"name": "Slime", "health": 15.0, "attack": 2.5, "defense": 2.5},
         {"name": "Ogre", "health": 30.0, "attack": 5.0, "defense": 1.5},
+        {"name": "Bat", "health": 5.0, "attack": 8.0, "defense": 5},
+        {"name": "Worm", "health": 20.0, "attack": 3.0, "defense": 10},
+        {"name": "Rat", "health": 10.0, "attack": 4.0, "defense": 2},
+        {"name": "Spider", "health": 3.0, "attack": 10.0, "defense": 5.0},
     ],
     2: [
-        {"name": "Skeleton", "health": 8.5, "attack": 10.5, "defense": 0.5},
-        {"name": "Witch", "health": 15.0, "attack": 5.0, "defense": 3},
+        {"name": "Skeleton", "health": 8.5, "attack": 8.5, "defense": 0.5},
+        {"name": "Witch", "health": 15.0, "attack": 10.0, "defense": 1},
+        {"name": "Ghost", "health": 5.0, "attack": 5.0, "defense": 20},
+        {"name": "Armored Skeleton", "health": 25.0, "attack": 13.0, "defense": 10},
+        {"name": "Wizard", "health": 15.0, "attack": 8.5, "defense": 0.5},
+        {"name": "Ranger Skeleton", "health": 10, "attack": 10, "defense": 0.25},
     ],
 }
